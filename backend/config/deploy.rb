af8e90cc45b2512abe8f56d4b26c491b50349244
Michaël Hoste <michael.hoste@gmail.com>
2017-07-28 17:36:19 +0200

    add capistrano configurations

---
+++ backend/config/deploy.rb
@@ -0,0 +1,15 @@
+lock '3.8.2'
+
+set :repo_url, 'git@github.com:micbelgique/covfefe-desk.git'
+set :repo_tree, 'backend'
+
+set :keep_releases, 10
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle private/system public/uploads}
+
+set :rbenv_type, 'user'
+set :rbenv_ruby,  File.read('.ruby-version').strip
+
+#after  'deploy:publishing',        'delayed_job:restart'
+#before 'deploy:assets:precompile', 'deploy:npm:install'
