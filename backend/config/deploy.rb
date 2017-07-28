d07f3baf59f18a743d87398967d5264611693605
Michaël Hoste <michael.hoste@gmail.com>
2017-07-28 17:52:58 +0200

    better config

--- backend/config/deploy.rb
+++ backend/config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_tree, 'backend'
 set :keep_releases, 10
 
 set :linked_files, %w{config/database.yml}
-set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle private/system public/uploads}
+set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle private/system public/system}
 
 set :rbenv_type, 'user'
 set :rbenv_ruby,  File.read('.ruby-version').strip
