825ef50ad1d7f379a3ce9e4867127ffe4b51f4fc
Simon Depelchin <dep.simon@gmail.com>
2017-07-28 16:05:13 +0200

    Installation bootstrap et jquery

--- backend/app/javascript/packs/application.js
+++ backend/app/javascript/packs/application.js
@@ -8,3 +8,9 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
+
+try {
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap-sass');
+} catch (e) {}
