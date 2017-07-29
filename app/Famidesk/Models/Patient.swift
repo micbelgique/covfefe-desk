8ca8df052cb210bfc2143b190f62bbed490f1fdd
Mathias <matsou03@gmail.com>
2017-07-29 10:49:00 +0200

    Mobile - Prepare models

---
+++ app/Famidesk/Models/Patient.swift
@@ -0,0 +1,19 @@
+//
+//  Patient.swift
+//  Famidesk
+//
+//  Created by Mathias Biard on 29/07/2017.
+//  Copyright © 2017 CodingOwl. All rights reserved.
+//
+
+import Foundation
+
+class Patient {
+
+    static func getPatientWithCode(code: String) -> Patient {
+        
+        // TODO
+        
+        return Patient()
+    }
+}
---
+++ app/Famidesk/Networks/URLHelper.swift
@@ -0,0 +1,15 @@
+//
+//  URLHelper.swift
+//  Famidesk
+//
+//  Created by Mathias Biard on 29/07/2017.
+//  Copyright © 2017 CodingOwl. All rights reserved.
+//
+
+import Foundation
+
+class URLHelper {
+    static func getUrlForQRCode(qrCodeContent: String) -> URL {
+        return URL(string: "http://famidesk.80limit.com/api/patients/yo.json?device_id=78A12647-4910-4162-9FC5-10A5EB259A75")!
+    }
+}
